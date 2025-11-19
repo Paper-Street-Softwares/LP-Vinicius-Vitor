80cd3983cf2749f7ea8d5b7658d7d84df7435d27
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-11-19 11:40:29 -0300

    fix: Separa Footer do Mapa

--- src/components/sectionElements/footer/LogoFooter.jsx
+++ src/components/sectionElements/footer/LogoFooter.jsx
@@ -9,7 +9,7 @@ function LogoFooter() {
         <img
           src={content.texts.navbar.logo.img}
           alt={content.texts.navbar.logo.alt}
-          className="max-h-[88px] w-auto "
+          className="max-h-[88px] w-auto p-2 bg-bgSectionDark rounded-md "
         ></img>
       </div>
     </MotionDivDownToUp>
--- src/components/sections/FooterSocial.jsx
+++ src/components/sections/FooterSocial.jsx
@@ -36,8 +36,8 @@ export default function FooterSocial({
       textColor = "text-black";
       break;
     default:
-      footerBackground = "bg-bgSectionDark";
-      textColor = "text-white";
+      footerBackground = "bg-white";
+      textColor = "text-black";
       break;
   }
 
